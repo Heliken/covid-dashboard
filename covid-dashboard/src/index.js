b1e475da4cff99299fea2fc3bb91f06a2e888b77
Heliken <burshf@gmail.com>
2020-12-20 00:32:04 +0300

    feat: remove init.js

--- covid-dashboard/src/index.js
+++ covid-dashboard/src/index.js
@@ -1,5 +1,11 @@
 import './styles/index.scss';
-import "@babel/polyfill";
-import init from './js/modules/init.js';
+import '@babel/polyfill';
+import insertAppTemplate from './js/modules/insertAppTemplate.js';
+import addImages from './js/modules/addImages.js';
+import Table1 from './js/modules/Table1.js';
+import chartBlock from './js/modules/chartBlock.js';
 
-init();
+insertAppTemplate();
+addImages();
+chartBlock.init();
+const appTable1 = new Table1();
