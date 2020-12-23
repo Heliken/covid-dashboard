8ce79c53025f1d5d385c1a438aa518d1f811d8a2
Heliken <burshf@gmail.com>
2020-12-24 02:20:54 +0300

    fix: update styles

--- covid-dashboard/src/js/modules/uiActions.js
+++ covid-dashboard/src/js/modules/uiActions.js
@@ -2,8 +2,10 @@ export default () => {
   const fullscreenBtn = document.querySelectorAll('.section__fullscreen-button');
   fullscreenBtn.forEach((btn) => {
     const section = btn.closest('.section');
+    const sectionParent = section.parentNode;
     btn.addEventListener('click', () => {
       section.classList.toggle('section--fullscreen');
+      sectionParent.classList.toggle('section-fullscreen-parent');
     });
   });
 };
