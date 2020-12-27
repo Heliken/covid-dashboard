69b778c582c39840c7d4172a7aceccb9d1a1efdc
Ilya Komar <helias@yandex.ru>
2020-12-28 01:49:02 +0300

    fix: correct ArtiFisher remarks

--- covid-dashboard/src/js/modules/map.js
+++ covid-dashboard/src/js/modules/map.js
@@ -23,6 +23,7 @@ export default () => {
   L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
+    minZoom: 2,
     id: 'mapbox/light-v10',
     tileSize: 512,
     zoomOffset: -1,
